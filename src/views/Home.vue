02751e8a512fbc6c9876a684df0c5806ab4e55dd
estelikm973 <m.e.83dev@gmail.com>
2023-03-03 22:34:59 +0700

    Refactor code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -54,66 +54,36 @@ const getAveragePoint = (partIdx: number) => {
     : Number(totalPointArr[partIdx] / totalAnswerArr[partIdx]).toFixed(2);
 };
 
-const statistics = computed(() => [
-  {
-    title: "Điểm TB",
-    stats:
-      totalAnswers.value > 0
-        ? Number(totalPoints.value / totalAnswers.value).toFixed(2)
-        : "---",
-    icon: "mdi-trophy",
-    color: "success",
-  },
-  {
-    title: "Số câu đã làm",
-    stats: totalAnswers.value,
-    icon: "mdi-history",
-    color: "primary",
-  },
+const statistics = computed(() => {
+  const arr: any[] = [
+    {
+      title: "Điểm TB",
+      stats:
+        totalAnswers.value > 0
+          ? Number(totalPoints.value / totalAnswers.value).toFixed(2)
+          : "---",
+      icon: "mdi-trophy",
+      color: "success",
+    },
+    {
+      title: "Số câu đã làm",
+      stats: totalAnswers.value,
+      icon: "mdi-history",
+      color: "primary",
+    },
+  ];
+  for (let i = 1; i <= 6; i++) {
+    arr.push({
+      title: "Điểm TB phần " + i,
+      stats: getAveragePoint(i - 1),
+      answers: totalAnswerArr[i - 1],
+      icon: `mdi-numeric-${i}-box`,
+      color: "warning",
+    });
+  }
 
-  {
-    title: "Điểm TB phần 1",
-    stats: getAveragePoint(0),
-    answers: totalAnswerArr[0],
-    icon: "mdi-numeric-1-box",
-    color: "warning",
-  },
-  {
-    title: "Điểm TB phần 2",
-    stats: getAveragePoint(1),
-    answers: totalAnswerArr[1],
-    icon: "mdi-numeric-2-box",
-    color: "warning",
-  },
-  {
-    title: "Điểm TB phần 3",
-    stats: getAveragePoint(2),
-    answers: totalAnswerArr[2],
-    icon: "mdi-numeric-3-box",
-    color: "warning",
-  },
-  {
-    title: "Điểm TB phần 4",
-    stats: getAveragePoint(3),
-    answers: totalAnswerArr[3],
-    icon: "mdi-numeric-4-box",
-    color: "warning",
-  },
-  {
-    title: "Điểm TB phần 5",
-    stats: getAveragePoint(4),
-    answers: totalAnswerArr[4],
-    icon: "mdi-numeric-5-box",
-    color: "warning",
-  },
-  {
-    title: "Điểm TB phần 6",
-    stats: getAveragePoint(5),
-    answers: totalAnswerArr[5],
-    icon: "mdi-numeric-6-box",
-    color: "warning",
-  },
-]);
+  return arr;
+});
 
 const addAnswersDialog = ref(false);
 const newAnswersStr = ref("");
@@ -153,7 +123,12 @@ const onAddAnswersSave = async () => {
           <VCol v-for="item in statistics" :key="item.title" cols="6" sm="6">
             <div class="d-flex align-center">
               <div class="me-3">
-                <VAvatar :color="item.color" rounded size="42" class="elevation-1">
+                <VAvatar
+                  :color="item.color"
+                  rounded
+                  size="42"
+                  class="elevation-1"
+                >
                   <VIcon size="24" :icon="item.icon" />
                 </VAvatar>
               </div>
@@ -163,7 +138,10 @@ const onAddAnswersSave = async () => {
                   {{ item.title }}
                 </span>
                 <div>
-                  <span class="font-weight-medium d-inline-block mr-1" style="font-size: 20px">
+                  <span
+                    class="font-weight-medium d-inline-block mr-1"
+                    style="font-size: 20px"
+                  >
                     {{ item.stats }}
                   </span>
                   <span v-if="item.answers">{{ `(${item.answers} lần)` }}</span>
@@ -176,7 +154,9 @@ const onAddAnswersSave = async () => {
     </VCard>
 
     <VCard title="Thêm điểm số" class="mt-4">
-      <VCardText> Thêm ngay điểm số của các câu hỏi trong quá trình luyện tập. </VCardText>
+      <VCardText>
+        Thêm ngay điểm số của các câu hỏi trong quá trình luyện tập.
+      </VCardText>
       <VCardActions>
         <VBtn block color="primary" variant="tonal" @click="onAddAnswersClick">
           Thêm điểm
@@ -185,9 +165,17 @@ const onAddAnswersSave = async () => {
     </VCard>
 
     <VCard title="Tạo một bộ đề thi ngẫu nhiên" class="mt-4">
-      <VCardText> Hệ thống sẽ tạo ra một bộ đề thi ngẫu nhiên theo cấu trúc đề thi thật từ bộ GTVT. </VCardText>
+      <VCardText>
+        Hệ thống sẽ tạo ra một bộ đề thi ngẫu nhiên theo cấu trúc đề thi thật từ
+        bộ GTVT.
+      </VCardText>
       <VCardActions>
-        <VBtn block color="primary" variant="tonal" :to="{ name: 'CreateRandomExam' }">
+        <VBtn
+          block
+          color="primary"
+          variant="tonal"
+          :to="{ name: 'CreateRandomExam' }"
+        >
           Tạo ngay
         </VBtn>
       </VCardActions>
@@ -197,15 +185,23 @@ const onAddAnswersSave = async () => {
       <v-card title="Thêm điểm số">
         <v-card-item>
           <span class="text-caption">
-            Thêm điểm số mới bằng cách nhập vào ô dưới đây theo cấu trúc: 10:3, 25:5 <br /> trong đó 10 là câu hỏi,
-            3 là điểm số tương ứng <br /> và các câu được ngăn cách bằng dấu phẩy.
+            Thêm điểm số mới bằng cách nhập vào ô dưới đây theo cấu trúc: 10:3,
+            25:5 <br />
+            trong đó 10 là câu hỏi, 3 là điểm số tương ứng <br />
+            và các câu được ngăn cách bằng dấu phẩy.
           </span>
         </v-card-item>
         <v-card-text>
-          <VTextarea variant="outlined" v-model="newAnswersStr" placeholder="10:3, 25:5" />
+          <VTextarea
+            variant="outlined"
+            v-model="newAnswersStr"
+            placeholder="10:3, 25:5"
+          />
         </v-card-text>
         <v-card-actions class="justify-end">
-          <VBtn color="primary" variant="tonal" @click="onAddAnswersSave">Lưu</VBtn>
+          <VBtn color="primary" variant="tonal" @click="onAddAnswersSave"
+            >Lưu</VBtn
+          >
           <VBtn @click="addAnswersDialog = false">Hủy bỏ</VBtn>
         </v-card-actions>
       </v-card>
