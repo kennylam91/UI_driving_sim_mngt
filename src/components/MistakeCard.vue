c662ed259abbf4ed238cfa70d1a10367654cbacc
lampv <lam.phamvan@emea.nttdata.com>
2023-03-07 18:09:00 +0700

    Fix MistakeCard

--- src/components/MistakeCard.vue
+++ src/components/MistakeCard.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
-import {useAppStore} from "@/store/app";
-import {storeToRefs} from "pinia";
-import {computed, reactive, ref, watch} from "vue";
-import {Answer} from "@/common/type";
+import { useAppStore } from "@/store/app";
+import { storeToRefs } from "pinia";
+import { computed, reactive, ref, watch } from "vue";
+import { Answer } from "@/common/type";
+import { addAnswers } from "@/services/answer.service";
 
 const appStore = useAppStore();
-const {loggedInUser} = useAppStore();
-const {answersByQuestionMap, loading} = storeToRefs(appStore);
+const { loggedInUser, fetchAnswers } = appStore;
+const { answersByQuestionMap, loading } = storeToRefs(appStore);
 
 const minPointQuestions: any[] = reactive([null, null, null, null, null, null]);
 
 watch(loading, (newVal) => {
   if (!newVal) {
-    console.log("recalculate the recently mistakes");
+    for (let i = 0; i < 6; i++) {
+      minPointQuestions[i] = null;
+    }
 
     answersByQuestionMap.value.forEach((value, key) => {
       const found = minPointQuestions[value.part];
@@ -29,49 +32,49 @@ watch(loading, (newVal) => {
   }
 });
 
-const practiceDialog = ref(false)
-const answers = ref<Answer[]>([])
+const practiceDialog = ref(false);
+const answers = ref<Answer[]>([]);
 const onPracticeClick = () => {
-  practiceDialog.value = true
-  answers.value = minPointQuestions.map(item => ({
+  practiceDialog.value = true;
+  answers.value = minPointQuestions.map((item) => ({
     question: item.question,
     point: null,
-    username: loggedInUser.usernam
-  }))
-}
-const isSaveBtnDisabled = computed(() => false)
-const save = () => {
+    username: loggedInUser.username,
+  }));
+};
+const isSaveBtnDisabled = computed(() => false);
 
-}
-
+const save = async () => {
+  await addAnswers(answers.value.filter((item) => item.point !== null));
+  practiceDialog.value = false;
+  await fetchAnswers();
+};
 </script>
 <template>
   <VCard title="Tình huống cần cải thiện">
     <v-table density="compact" fixed-header height="260px">
       <thead>
-      <tr>
-        <th>Phần</th>
-        <th>Tình huống</th>
-        <th>Điểm</th>
-        <th>TB</th>
-      </tr>
+        <tr>
+          <th>Phần</th>
+          <th>Tình huống</th>
+          <th>Điểm</th>
+          <th>TB</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="(questionObj, index) in minPointQuestions" :key="index">
-        <template v-if="questionObj">
-          <td>{{ questionObj && questionObj.part + 1 }}</td>
-          <td>{{ questionObj && questionObj.question }}</td>
-          <td>{{ questionObj && questionObj.answers.join(", ") }}</td>
-          <td>{{ questionObj && questionObj.avg }}</td>
-        </template>
-      </tr>
+        <tr v-for="(questionObj, index) in minPointQuestions" :key="index">
+          <template v-if="questionObj">
+            <td>{{ questionObj && questionObj.part + 1 }}</td>
+            <td>{{ questionObj && questionObj.question }}</td>
+            <td>{{ questionObj && questionObj.answers.join(", ") }}</td>
+            <td>{{ questionObj && questionObj.avg }}</td>
+          </template>
+        </tr>
       </tbody>
     </v-table>
     <v-divider></v-divider>
     <v-card-actions>
-      <v-btn color="warning" variant="text">
-        Xem thêm
-      </v-btn>
+      <!--      <v-btn color="warning" variant="text"> Xem thêm</v-btn>-->
       <v-btn color="primary" variant="text" @click="onPracticeClick">
         Luyện tập ngay
       </v-btn>
@@ -83,38 +86,42 @@ const save = () => {
           color="warning"
           title="Luyện tập các tình huống sai"
         >
-          <VCardText>
-            <VForm>
-              <VRow dense>
-                <VCol v-for="answer in answers" :key="answer.question" cols="6">
-                  <VSelect
-                    v-model="answer.point"
-                    :items="[5, 4, 3, 2, 1, 0]"
-                    :label="`Câu ${answer.question}`"
-                    chips
-                  />
-                </VCol>
-              </VRow>
-              <VRow dense>
-                <VCol>
-                  <div class="form-action">
-                    <VBtn
-                      color="primary"
-                      variant="elevated"
-                      :disabled="isSaveBtnDisabled"
-                      @click="save"
-                    >
-                      Lưu
-                    </VBtn>
-                    <v-btn variant="plain" class="ml-2" to="/">Hủy bỏ</v-btn>
-                  </div>
-                </VCol>
-              </VRow>
-            </VForm>
-          </VCardText>
         </VToolbar>
+        <VCardText>
+          <VForm>
+            <VRow dense>
+              <VCol v-for="answer in answers" :key="answer.question" cols="6">
+                <VSelect
+                  v-model="answer.point"
+                  :items="[5, 4, 3, 2, 1, 0]"
+                  :label="`Câu ${answer.question}`"
+                  chips
+                />
+              </VCol>
+            </VRow>
+            <VRow dense>
+              <VCol>
+                <div class="form-action">
+                  <VBtn
+                    color="primary"
+                    variant="elevated"
+                    :disabled="isSaveBtnDisabled"
+                    @click="save"
+                  >
+                    Lưu
+                  </VBtn>
+                  <v-btn
+                    variant="plain"
+                    class="ml-2"
+                    @click="() => (practiceDialog = false)"
+                    >Hủy bỏ
+                  </v-btn>
+                </div>
+              </VCol>
+            </VRow>
+          </VForm>
+        </VCardText>
       </VCard>
     </VDialog>
   </VCard>
-
 </template>
